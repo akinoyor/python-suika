d2c511e2809dbe0e2a9b5e3c5901cdd63414e143
akinoyor <155416793+akinoyor@users.noreply.github.com>
2024-07-01 18:05:41 +0900

    random

--- suika.py
+++ suika.py
@@ -1,14 +1,19 @@
 import math
-
-suika = [10, 10]#仮設定　最終的にはRandamを用いて行う
-player = [7, 6]#プレイヤー初期値
+import random
+suikaX = random.randrange(0, 10)
+suikaY = random.randrange(0, 10)
+playerX = random.randrange(0, 10)
+playerY = random.randrange(0, 10)
+suika = [suikaX, suikaY]#仮設定　最終的にはRandamを用いて行う
+player = [playerX, playerY]#プレイヤー初期値
 clearMessage = 'Congratulations on the correct answer'
+
 print(player == suika)
-if player == suika:
-    print(clearMessage)
-else:
+while player != suika:
     dx = suika[0] - player[0]
     dy = suika[1] - player[1]
     print(dx, dy)
     distance = math.sqrt(dx**2 + dy**2)
     print(distance)
+
+print(clearMessage)
