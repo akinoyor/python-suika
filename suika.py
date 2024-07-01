d84ecae5f9cd526179c18c8fb4a737739bac561c
akinoyor <155416793+akinoyor@users.noreply.github.com>
2024-07-01 18:39:00 +0900

    [fix]touple

--- suika.py
+++ suika.py
@@ -1,19 +1,18 @@
 import math
 import random
 BOARD_SIZE = 10
-suika_x = random.randrange(0, BOARD_SIZE)
-suika_y = random.randrange(0, BOARD_SIZE)
-player_x = random.randrange(0, BOARD_SIZE)
-player_y = random.randrange(0, BOARD_SIZE)
+suika = (random.randrange(0, BOARD_SIZE), random.randrange(0, BOARD_SIZE))
+player = (random.randrange(0, BOARD_SIZE), random.randrange(0, BOARD_SIZE))
 
 CLEAR_MESSAGE = 'Congratulations on the correct answer'
 
-while (player_x != suika_x)or(player_y != suika_y):
-    dx = suika_x - player_x
-    dy = suika_y - player_y
+while (player != suika):
+    dx = suika[0] - player[0]
+    dy = suika[1] - player[1]
     distance = math.sqrt(dx**2 + dy**2)
-    print('プレイヤーとスイカの距離',distance)
+    print('プレイヤーとスイカの距離', distance)
     move = input('N:北へ一歩　E:東へ一歩　S:南へ一歩　W:西へ一歩 どちらに動くか入力してください')
+    player_x, player_y = player
     if(move == 'N'):
         player_y = player_y + 1
     elif(move == 'E'):
@@ -24,5 +23,7 @@ while (player_x != suika_x)or(player_y != suika_y):
         player_x = player_x - 1
     else:
         print('大文字のNESWで入力してください')
+    
+    player = (player_x, player_y)
 
 print(CLEAR_MESSAGE)
