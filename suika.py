bae081c527a312a0b8fafc166289fe7ba6342750
akinoyor <155416793+akinoyor@users.noreply.github.com>
2024-07-01 18:31:55 +0900

    [fix]names

--- suika.py
+++ suika.py
@@ -1,27 +1,28 @@
 import math
 import random
-suikaX = random.randrange(0, 10)
-suikaY = random.randrange(0, 10)
-playerX = random.randrange(0, 10)
-playerY = random.randrange(0, 10)
+BOARD_SIZE = 10
+suika_x = random.randrange(0, BOARD_SIZE)
+suika_y = random.randrange(0, BOARD_SIZE)
+player_x = random.randrange(0, BOARD_SIZE)
+player_y = random.randrange(0, BOARD_SIZE)
 
-clearMessage = 'Congratulations on the correct answer'
+CLEAR_MESSAGE = 'Congratulations on the correct answer'
 
-while (playerX != suikaX)or(playerY != suikaY):
-    dx = suikaX - playerX
-    dy = suikaY - playerY
+while (player_x != suika_x)or(player_y != suika_y):
+    dx = suika_x - player_x
+    dy = suika_y - player_y
     distance = math.sqrt(dx**2 + dy**2)
     print('プレイヤーとスイカの距離',distance)
     move = input('N:北へ一歩　E:東へ一歩　S:南へ一歩　W:西へ一歩 どちらに動くか入力してください')
     if(move == 'N'):
-        playerY = playerY + 1
+        player_y = player_y + 1
     elif(move == 'E'):
-        playerX = playerX + 1
+        player_x = player_x + 1
     elif(move == 'S'):
-        playerY = playerY - 1
+        player_y = player_y - 1
     elif(move == 'W'):
-        playerX = playerX - 1
+        player_x = player_x - 1
     else:
         print('大文字のNESWで入力してください')
 
-print(clearMessage)
+print(CLEAR_MESSAGE)
